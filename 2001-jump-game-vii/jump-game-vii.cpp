6a7f8341e65afa2032710e719e4716f4dbccae69
Krishna Singh <142901299+Krishna-singh18@users.noreply.github.com>
2026-05-25 12:04:47 +0530

    Time: 9 ms (61.62%) | Memory: 29.1 MB (28.05%) - LeetSync

---
+++ 2001-jump-game-vii/jump-game-vii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+
+        int n = s.size();
+
+        vector<int> dp(n, 0);
+        dp[0] = 1;
+
+        int pre = 0;
+
+        for (int i = 1; i < n; i++) {
+
+            if (i >= minJump)
+                pre += dp[i - minJump];
+
+            if (i > maxJump)
+                pre -= dp[i - maxJump - 1];
+
+            dp[i] = (pre > 0 && s[i] == '0');
+        }
+
+        return dp[n - 1];
+    }
+};
